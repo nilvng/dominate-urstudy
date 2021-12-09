98cc0793efe4cbb0b027b8254ff06dd1e8417f1f
Nil Nguyen <nilverspace@gmail.com>
2021-12-09 11:46:30 +0700

    seperate js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const { stat } = require('fs/promises');
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
 
 const {Server } = require("socket.io");
 const io = new Server(server);
-
+app.use("/client",express.static("./client/"))
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html')
 })
